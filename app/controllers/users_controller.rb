009a7f725fa257558df30a8b3a432902acf7af22
Kojin Oshiba <kojin.oshiba@gmail.com>
2016-08-20 23:38:38 -0700

    implement signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+    @signup_header = true
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to MentorMatch! This is your profile. Update to describe yourself!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
