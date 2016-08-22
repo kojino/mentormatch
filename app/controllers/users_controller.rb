6a2e271c168093526086aab6778a232a9a6d09bf
Kojin Oshiba <kojin.oshiba@gmail.com>
2016-08-21 21:12:27 -0700

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to MentorMatch! This is your profile. Update to describe yourself!"
       redirect_to @user
     else
